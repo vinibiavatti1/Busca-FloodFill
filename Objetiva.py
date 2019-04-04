c4e0f52d040f97e4a098529c7eb01f5f121ea53b
Vinicius Reif <vinicius.reif@senior.com.br>
2019-04-04 09:46:06 -0300

    Adicionado comentarios

--- Objetiva.py
+++ Objetiva.py
@@ -1,15 +1,39 @@
-#
+# FURB Universidade Regional de Blumenau
+# 
+# Algoritmo de busca para inteligência artificial objetiva
+# baseado no algoritmo de pathfinding floodfill.
+# 
+# Alunos:
+# Vinícius Reif Biavatti
+# Bryan Leite
+
 # Importações
-#
 from tkinter import *
 from copy import copy, deepcopy
 import time
 import random
 
 
+# Classe Node. Esta classe serve para armazenar as posições
+# de cada nó da matriz e também armazenar a referência para
+# o nó pai. Por exemplo, dada a matriz abaixo, o caminho
+# estabelecido por uma lista de nós seria:
+# 1 - Agente
+# 2 - Sugeira
+# 
+# 1, 0, 0, 0
+# 0, 0, 2, 0
+# 0, 0, 0, 0
+# 0, 0, 0, 0
 #
-# Classe Nó
+# Lista de nós:
+# Nó 1 (x = 0, y = 0)
+# Nó 2 (x = 1, y = 0, pai = Nó 1)
+# Nó 3 (x = 2, y = 0, pai = Nó 2)
+# Nó 4 (x = 2, y = 1, pai = Nó 3)
 #
+# Com isto, podemos percorrer a lista até a origem para obter
+# um caminho
 class Node:
     def __init__(self, x, y, parent=None):
         self.x = x
@@ -35,9 +59,15 @@ class Node:
         return self.parent
 
 
+# Gerar um mapa em matriz 4x4 com sugeiras aleatórias. A chance de cada
+# posição da matriz para se ter uma sugeira é de 25%. Um exemplo de retorno
+# de matriz é:
+# 1 - Sugeira
 #
-# Gerar mapa com sugeira aleatório
-#
+# 0, 1, 0, 0
+# 0, 0, 0, 1
+# 0, 0, 0, 0
+# 0, 0, 1, 0
 def generate_map():
     map = [[None, None, None, None],
            [None, None, None, None],
@@ -54,38 +84,65 @@ def generate_map():
                 map[i][j] = 0
     return map
 
+# Variáveis Globais. Abaixo segue uma coleção de variáveis utilizadas
+# no algoritmo. Estas variáveis são acessadas por todos os métodos
+# deste arquivo
 
-#
-# Globais
-#
+# Mapa (Matriz 4x4)
 map = generate_map()
 
-#Mapa Manual
+# Mapa Manual (Matriz 4x4)
 #map = [[0,0,0,0],
 #       [1,0,0,1],
 #       [0,1,0,0],
 #       [0,0,0,1]]
 
+# Cópia de mapa para realização da busca
 process_map = deepcopy(map)
+
+# Cópia de mapa para renderização da tela
 window_map = deepcopy(map)
+
+# Pilha utilizada para busca
 list = [Node(0, 0)]
+
+# Lista com todos os nós contendo a solução do caminho mais próximo
 solution = [Node(0,0)]
+
+# Tempo para cada movimento do agente
 delay = 0.5
+
+# Largura de cada posição da grade
 tile_width = 100
+
+# Altura de cada posição da grade
 tile_height = 100
+
+# Cores
 floor_color = "#CCCCCC"
 dirt_color = "orange"
 robot_color = "blue"
+
+# Variáveis para armazenar posição do agente
 x = 0
 y = 0
 x_pos = x
 y_pos = y
+
+# Índice de interação da lista de solução
 index_solution = 0
+
+# Pontuação
 score = 0
 
+# Esta rotina verifica se o mapa (Matriz) está sem nenhuma sugeira. Caso existir
+# uma sugeira, a função retorna False, senão True.
+# Exemplo de retorno True da função:
 #
-# Verifica se o mapa está limpo
-#
+# 0, 0, 0, 0
+# 0, 0, 0, 0
+# 0, 0, 0, 0
+# 0, 0, 0, 0
 def map_clean():
     for i in range(len(map)):
         for j in range(len(map[i])):
@@ -94,9 +151,8 @@ def map_clean():
     return True
 
 
-#
-# Imprimir matriz
-#
+# Mostrar matriz no console. Esta função é utilizada somente para fins de
+# depuração.
 def print_matrix(matrix):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
@@ -104,9 +160,13 @@ def print_matrix(matrix):
         print("")
 
 
+# Verificar se determinada posição da matriz existe. Retorna True caso
+# existir, senão False caso for uma posição inválida.
+# Exemplos de retorno:
 #
-# Verifica se existe posição
-#
+# has_position( 1, 2) : True
+# has_position( 4, 0) : False
+# has_position(-1, 2) : False
 def has_position(x, y):
     if (x < 0 or y < 0):
         return False
@@ -117,15 +177,41 @@ def has_position(x, y):
         return False
 
 
-#
 # Algoritmo Floodfill
+# Este algoritmo realiza a busca através de um objetivo da matriz e retorna 
+# um caminho formado por Nós (Classe Node) com o caminho mais próximo até a
+# sugeira mais próxima. O algoritmo utiliza uma pilha para armazenar os nós
+# que precisam ser processados. Para cada nós, é obtida as posições
+# adjacentes (x-1,y) (x+1,y) (x,y-1) (x,y+1). Cada um destes nós adjacentes
+# recebe como pai, o nó principal que está sendo processado. Caso uma
+# destas posições adjacentes possuirem uma sugeira, a função retorna este
+# nó, que possui como pai o caminho para a posição do robô. Por exemplo:
+# 1 - Sugeira
+# 2 - Agente
+# 3 - Rota
 #
+# Mapa
+# 2, 3, 3, 0
+# 0, 0, 3, 0
+# 0, 0, 1, 0
+# 0, 0, 0, 0
+#
+# Retorno:
+# Nó 1 (x = 0, y = 0)
+# Nó 2 (x = 1, y = 0, pai = Nó 1)
+# Nó 3 (x = 2, y = 0, pai = Nó 2)
+# Nó 4 (x = 2, y = 1, pai = Nó 3)
+# Nó 5 (x = 2, y = 2, pai = Nó 4)
 def floodfill():
+
+    # Interação pela pilha até encontrar uma sugeira, ou percorrer a matriz por
+	# inteiro
     while (len(list) != 0):
         node = list.pop(0)
         x = node.get_x()
         y = node.get_y()
 
+        # Verifica posição X - 1, Y
         if (has_position(x - 1, y)):
             new_node = Node(x - 1, y, node)
             if (process_map[x - 1][y] == 1):
@@ -134,6 +220,7 @@ def floodfill():
                 list.append(new_node)
                 process_map[x - 1][y] = 2
 
+        # Verifica posição X, Y - 1
         if (has_position(x, y - 1)):
             new_node = Node(x, y - 1, node)
             if (process_map[x][y - 1] == 1):
@@ -142,6 +229,7 @@ def floodfill():
                 list.append(new_node)
                 process_map[x][y - 1] = 2
 
+        # Verifica posição X + 1, Y
         if (has_position(x + 1, y)):
             new_node = Node(x + 1, y, node)
             if (process_map[x + 1][y] == 1):
@@ -150,6 +238,7 @@ def floodfill():
                 list.append(new_node)
                 process_map[x + 1][y] = 2
 
+        # Verifica posição X, Y + 1
         if (has_position(x, y + 1)):
             new_node = Node(x, y + 1, node)
             if (process_map[x][y + 1] == 1):
@@ -159,9 +248,9 @@ def floodfill():
                 process_map[x][y + 1] = 2
 
 
-#
-# Criar label na posicao
-#
+# Criar label na posicao determinada com largura e altura definidas
+# por parâmetro. Esta função é utilizada para renderizar a matriz
+# na tela.
 def make_label(master, x, y, h, w, *args, **kwargs):
     f = Frame(master, height=h, width=w)
     f.pack_propagate(0)  # don't shrink
@@ -171,9 +260,9 @@ def make_label(master, x, y, h, w, *args, **kwargs):
     return label
 
 
-#
-# Window Loop
-#
+# Loop executado pela biblioteca TKInter. Este loop renderiza o agente
+# na posição da da tela conforme a lista de solução. Caso o agente
+# encontrar uma sugeira, ela é limpa na tela
 def run():
     global count
     global index_solution
@@ -202,43 +291,69 @@ def run():
         window.after(100, run)
 
 
+# Início da execução do algoritmo. Fluxo de execução:
 #
-# Algoritmo
-#
+# 1 - Realiza a busca com floodfill partindo da posição do agente
+# 2 - Encontra a primeira sugeira e retorna o caminho mais curto até ela
+# 3 - Adiciona este caminho na lista de solução
+# 4 - Verifica se o mapa está limpo, se não estiver, executa a ação 1, 2 e 3 novamente
+# 5 - Se o mapa estiver limpo, a lista de solução estará completa, então a tela será renderizada
 print("Mapa")
 print_matrix(map)
+
+# Enquanto o mapa não estiver limpo, executa o algoritmo de busca
 while (not map_clean()):
     path = floodfill()
     x = path.get_x()
     y = path.get_y()
+	
+	# Lista auxiliar utilizada somente para armazenar a solução e e reverter
     aux_list = []
+	
+	# Interação pelo caminho de nós até chegar na posição inicial (posição do agente)
     while (path.get_parent() is not None):
+		# Destaca a posição na matriz para finds de depuração
         process_map[path.get_x()][path.get_y()] = 3
+		
+		# Incluir posição na lista auxiliar
         aux_list.append(path)
+		
+		# Obter pai
         path = path.get_parent()
+		
+	# Reverter lista para adicionar na lista de solução na ordem correta
     aux_list.reverse()
+	
+	# Adiciona na lista de solução
     solution.extend(aux_list)
-    #print_matrix(map)
+	
+	# Lista a sugeira
     map[x][y] = 0
+	
+	# Atribuir a posição nova do agente na pilha de nós
     list = [Node(x, y)]
+	
+	# Copia o mapa novamente com a sugeira limpa para executar o próximo processamento
     process_map = deepcopy(map)
 
-# Mostrar Solução
+# Mostrar Solução no console
 print("")
 print("Solução")
 for path in solution:
     print("%s %s" % (path.get_x(), path.get_y()))
 
+# Criar tela
 window = Tk()
 window.title("Inteligência Artificial - Objetiva")
 window.geometry('500x400')
 
+# Matriz de label conforme mapa 
 label_matrix = [[None, None, None, None],
                 [None, None, None, None],
                 [None, None, None, None],
                 [None, None, None, None]]
 
-# Grid
+# Gerar grid com labels e cores para representar Agente ou Sugeira
 for i in range(4):
     x = 0
     for j in range(4):
@@ -249,16 +364,15 @@ for i in range(4):
         x += 100
     y += 100
 
-# Pontos
+# Mostrar pontuação na tela
 l_score = make_label(window, 400, 0, tile_width, tile_height, background="white", text="10")
 l_score.config(font=("Arial", 44))
 
+# Executar loop de processamento da tela (Método run())
 count = 0.0
 run()
 window.mainloop()
 
-#
 # Main
-#
 if __name__ == '__main__':
     pass
